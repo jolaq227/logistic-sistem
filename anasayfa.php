403cc135f39d58143bdbb6cbc927fd17fd5e6b26
jolaq227 <mohamadsytr99@gmail.com>
2023-05-19 10:28:38 +0300

    anasayfa page security feature

--- anasayfa.php
+++ anasayfa.php
@@ -21,6 +21,14 @@ if (isset($_SESSION['kullanici_adi'])) {
         <?php
     }
 
+    $kullaniciS = getItem('kullanicilar', 'id', $_SESSION['ID']);
+
+    if ($kullaniciS['aktif'] === 2) {
+        $isAdmin = true;
+    } else {
+        $isAdmin = false;
+    }
+
 
     $baslad = date('Y-m-d 00:00:00', time());
     $bitisd = date('Y-m-d 23:59:59', time());
@@ -39,27 +47,29 @@ if (isset($_SESSION['kullanici_adi'])) {
     ?>
     <div class="anasayfa-icerik">
         <div class="buyuk-anasayfa">
-            <div>
-                <img src="iconlar/tl.png" alt="">
+            <?php if ($isAdmin) { ?>
                 <div>
-                    <p><?php echo lang('gununSatisToplamiTl'); ?></p>
-                    <span><?php echo isset($toplam) ? nokta($toplam) : '0'; ?> TL</span>
+                    <img src="iconlar/tl.png" alt="">
+                    <div>
+                        <p><?php echo lang('gununSatisToplamiTl'); ?></p>
+                        <span><?php echo isset($toplam) ? nokta($toplam) : '0'; ?> TL</span>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <img src="iconlar/card.png" alt="">
                 <div>
-                    <p><?php echo lang('depodaUrunSayisi'); ?></p>
-                    <span><?php echo isset($uSayisi) ? nokta($uSayisi) : '0'; ?></span>
+                    <img src="iconlar/card.png" alt="">
+                    <div>
+                        <p><?php echo lang('depodaUrunSayisi'); ?></p>
+                        <span><?php echo isset($uSayisi) ? nokta($uSayisi) : '0'; ?></span>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <img src="iconlar/urun.png" alt="">
                 <div>
-                    <p><?php echo lang('satilanUrunSayisi'); ?></p>
-                    <span><?php echo isset($toplamUrun) ? nokta($toplamUrun) : '0'; ?></span>
+                    <img src="iconlar/urun.png" alt="">
+                    <div>
+                        <p><?php echo lang('satilanUrunSayisi'); ?></p>
+                        <span><?php echo isset($toplamUrun) ? nokta($toplamUrun) : '0'; ?></span>
+                    </div>
                 </div>
-            </div>
+            <?php } else { echo '<div></div> <div></div> <div></div>'; } ?>
             <div>
                 <h4><center><?php echo lang('enCokSatilanlar'); ?></center></h4>
                 <table>
@@ -90,7 +100,8 @@ if (isset($_SESSION['kullanici_adi'])) {
                         <th><?php echo lang('toplam') . ' (TL)'; ?></th>
                     </tr>
                     <?php 
-                        $urunlerl = getAllFrom('satislar ORDER BY id DESC LIMIT 5');
+                        $sat    = $isAdmin ? '' : ' WHERE satisi_yapan = ' . $_SESSION['ID'];
+                        $urunlerl = getAllFrom('satislar ' . @$sat . ' ORDER BY id DESC LIMIT 5');
                         foreach($urunlerl as $urun) {
                             echo '<tr>
                                     <td>' . $urun['id'] . '</td>
